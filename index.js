f5b961431b7a1576851e4dc7c768d3303c981850
AnAlgoTrader <verita.codex@gmail.com>
2024-05-16 10:33:10 +0200

    .

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('message', (msg) => {
+        io.emit('message', msg);
+    });
+});
+
+server.listen(3001, () => {
+    console.log('listening on *:3001');
+});
